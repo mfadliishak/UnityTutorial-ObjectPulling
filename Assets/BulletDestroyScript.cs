254a721e21ad78b1f1bc69386428805565c7c2a3
Fadli Ishak <fadli@terato.com>
2015-11-06 15:44:37 +0800

    Initial Project with spaceship shooting endless bullets.
    The ship will instatiate bullet every .05f time.
    The bullet will move itself and destroy itself.
    
    DONT do this!! It is so expensive.

---
+++ Assets/BulletDestroyScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletDestroyScript : MonoBehaviour {
+	
+	void Start () 
+	{
+		Destroy (gameObject, 2f);
+	}
+
+}
---
+++ Assets/BulletFireScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletFireScript : MonoBehaviour {
+
+	public float fireTime = .05f;
+	public GameObject bullet;
+	
+	void Start () 
+	{
+		InvokeRepeating ("Fire", fireTime, fireTime);
+	}
+
+	void Fire () 
+	{
+		Instantiate (bullet, GetComponent<Transform> ().position, Quaternion.identity);
+	}
+}
